8cc76bd5a212ac5cf2d965a06a6565200d3e3b9c
Ayush Khamrui <ayushkhamrui@Ayushs-MacBook-Air.local>
2025-08-07 20:49:37 +0530

    almost all changes except checkup

--- main.py
+++ main.py
@@ -4,6 +4,14 @@ from app.api import chatbot
 
 app = FastAPI()
 
+app.add_middleware(
+    "fastapi.middleware.cors.CORSMiddleware",
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(auth.router, prefix="/auth", tags=["auth"])
 app.include_router(checkup.router, prefix="/checkup", tags=["checkup"])
 app.include_router(doclist.router, prefix="/doclist", tags=["doclist"])
